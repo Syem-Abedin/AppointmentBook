6984a9bfc8e49eeb3f385fbb9c2083626c107263
SyemAbedin <Syema@nycstudents.net>
2025-10-03 15:32:22 -0400

    tried to debug, worked but didnt use whatever I debugged in findFreeBlock

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,19 +1,23 @@
 public class AppointmentBook {
-    private boolean [][] schedule;
-    public AppointmentBook(boolean [][] schedule) {
+    private boolean[][] schedule;
+
+    public AppointmentBook(boolean[][] schedule) {
         this.schedule = schedule;
     }
+
     private boolean isMinuteFree(int period, int minute) {
-        return schedule [period - 1][minute];
+        return schedule[period - 1][minute];
     }
     public int findFreeBlock(int period, int duration) {
         int block = 0;
-        for(int i = 0; i < 60; i++)
-            if(isMinuteFree(period, i)) {
+        for (int i = 0; i < 60; i++)
+            if (isMinuteFree(period, i)) {
                 block++;
-                if(block == duration)
+                if (block == duration)
                     return i - duration + 1;
             }
+            else block = 0;
+        return -1;
     }
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
         return false;
