208d447654b971ed10e8a4e16d26c1d418776c2b
Syem <syema@nycstudents.net>
2025-10-05 22:48:57 -0400

    started working on assignment at home

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    boolean [][] schedule = new boolean [8][60];
-    AppointmentBook a = new AppointmentBook(schedule);
-    for(int i = 10; i < 15; i++) schedule[1][i] = true;
+    public static void main(String[] args) {
+        boolean [][] schedule = new boolean [8][60];
+        AppointmentBook a = new AppointmentBook(schedule);
+        for(int i = 25; i < 30;i++) schedule[1][i] = true;
+        for(int i = 10; i < 15; i++) schedule[2][i] = true;
+        for(int i = 41; i < 60;i++) schedule[2][i] = true;
+        for(int i = 5; i < 30;i++) schedule[3][i] = true;
+        for(int i = 44; i < 60;i++) schedule[3][i] = true;
+        a.printPeriod(2);
+        a.findFreeBlock(2, 15);
+        AppointmentBook b = new AppointmentBook(schedule);
+        int period = 2; while(period < 5)
+        {
+            System.out.print("Period: " + period);
+            b.printPeriod(period);
+            period++;
+        }
+        b.printPeriod(4);
+        System.out.println(b.makeAppointment(2, 4, 22));
+        b.printPeriod(4);
+    }
 }
